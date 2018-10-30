14fb77386d7402237867dec3a3116b273681dac4
MateuszMarczuk <mateuszmarczuk95@gmail.com>
2018-10-30 09:58:31 +0100

    Widok okna rejestracji

---
+++ src/sample/controller/SignupController.java
@@ -0,0 +1,59 @@
+package sample.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Mateusz Marczuk
+ * Wojskowa Akademia Techniczna im. Jarosława Dąbrowskiego, Warszawa 30.10.2018.
+ */
+public class SignupController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private CheckBox SignUpCheckboxMale;
+
+    @FXML
+    private TextField SignUpUsername;
+
+    @FXML
+    private TextField SignUpFirstName;
+
+    @FXML
+    private PasswordField SignUpPassword;
+
+    @FXML
+    private CheckBox SignUpCheckboxFemale;
+
+    @FXML
+    private TextField SignUpLastName;
+
+    @FXML
+    private TextField SignUpLocation;
+
+    @FXML
+    private Button SignUpButtonSignUp;
+
+    @FXML
+    void eeeeee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
